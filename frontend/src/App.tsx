7e11610b40968810098b52a57dc3da849eaf0701
Nyanduck72 <jesusjagl2004@gmail.com>
2025-02-15 23:11:50 -0600

    Linked backend to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,39 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Switch, Route } from "wouter";
 import Register from "./pages/register";
 import LogIn from "./pages/logIn";
 
 function App() {
+  const [isJumpscared, setIsJumpscared] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Ensure the Egg library is loaded before using it
+    if (window.Egg) {
+      const egg = new window.Egg();
+      egg.addCode("f,u,g,a,a,l,c,a,r,l,s", () => {
+        setIsJumpscared(true);
+        const t = setTimeout(() => {
+          setIsJumpscared(false);
+        }, 4000); // Display the jumpscare for 1 second
+        return () => clearTimeout(t);
+      });
+      egg.listen();
+    }
+  }, []);
+
   return (
     <>
+      {isJumpscared && (
+        <div className="jumpscare-container">
+          <div className="jumpscare-content"></div>
+        </div>
+      )}
       <Switch>
         <Route path={"/"}></Route>
-        <Route path={"/login"} component={LogIn}>Log in</Route>
+        <Route path={"/login"} component={LogIn}>
+          Log in
+        </Route>
         <Route path={"/register"} component={Register}></Route>
       </Switch>
     </>
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,13 +1,35 @@
-import RegisterForm from "../components/registerForm"
+import { useState, useEffect } from "react";
+import RegisterForm from "../components/registerForm";
 
 const Register = () => {
+  const [isJumpscared, setIsJumpscared] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Ensure the Egg library is loaded before using it
+    if (window.Egg) {
+      const egg = new window.Egg();
+      egg.addCode("m,b,a,t,r,e,s,@,u,t,c,h,.,e,d,u,.,m,x", () => {
+        setIsJumpscared(true);
+        const t = setTimeout(() => {
+          setIsJumpscared(false);
+        }, 5000); // Display the jumpscare for 1 second
+        return () => clearTimeout(t);
+      });
+      egg.listen();
+    }
+  }, []);
   return (
     <>
-        <div className="w-screen h-screen bg-neutral-700 flex justify-center items-center">
-            <RegisterForm />
-        </div>
+      <div className="w-screen h-screen bg-neutral-700 flex justify-center items-center">
+        {isJumpscared && (
+          <div className="jumpscare-container">
+            <div className="jumpscare-content"></div>
+          </div>
+        )}
+        <RegisterForm />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
